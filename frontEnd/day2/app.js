4476f0ea4b4913f3f402da23094621bc8bc392bc
AhmedSayed14 <ahm.say14@gmail.com>
2023-05-24 04:51:57 +0300

    the up btn has been created successfully

--- frontEnd/day2/app.js
+++ frontEnd/day2/app.js
@@ -16,3 +16,39 @@
 //     reset.onclick =  ()=>{
 //         number.textContent = 0
 //     }
+
+
+// let name = document.querySelector(".name");
+// let email = document.querySelector(".email");
+// let password = document.querySelector(".password");
+
+// document.forms[0].onsubmit = function(e){
+//     nameValid = false;
+//     emailValid = false;
+//     passwordValid = false;
+
+//     if(password.value.length <8){
+        
+//     }
+
+//     if(nameValid === false || emailValid === false || passwordValid === false){
+//         e.preventDefault();
+//     }
+// }
+
+let upBtn = document.querySelector("button");
+
+window.onscroll = function(){
+    if(scrollY >=400){
+        upBtn.style.display = 'block';
+    }else{
+        upBtn.style.display = 'none';
+    }
+}
+
+upBtn.onclick = function(){
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    })
+}
